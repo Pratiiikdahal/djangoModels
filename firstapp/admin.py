cb6e9d05ffe0150a6b59e59f79e8fa4e29d696f7
Pratik Dahal <notpratiiiikdahal@gmail.com>
2025-11-11 21:01:22 +0545

    first commit

---
+++ firstapp/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from firstapp.models import *
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=['user','is_vendor','phonenumber','address','created_at']
+    list_per_page=15
+    
+admin.site.register(Profile,UserAdmin)
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display=['user','store_desc','store_logo','subscription_plan','subscription_expiry','created_at','updated_at']
+    list_per_page=15
+admin.site.register(Vendor,VendorAdmin)
+
+class CatAdmin(admin.ModelAdmin):
+    list_display=['cat_name','slug','parent']
+    list_per_page=15
+admin.site.register(Category,CatAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['product_name','product_description','brand','created_at']
+    list_per_page=15
+admin.site.register(Products,ProductAdmin)
+
+class VendorProductAdmin(admin.ModelAdmin):
+    list_display=['vendor','products','price','stock','listed_at']
+    list_per_page=15
+
---
+++ firstapp/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    is_vendor=models.BooleanField(default=False)
+    phonenumber=models.CharField(max_length=10)
+    address=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
+    
+class Vendor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    store_name=models.CharField(max_length=100)
+    store_desc=models.TextField(max_length=150,blank=True,null=True)
+    store_logo=models.ImageField(upload_to='vendor_image/',null=True,blank=True)
+    subscription_plan=models.CharField(max_length=50,choices=[('basic','Basic'),('premium','Premium'),('enterprise','Enterprise')],default='basic')
+    subscription_expiry=models.CharField(max_length=50,blank=True,null=True)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.store_name
+    
+class Category(models.Model):
+    cat_name=models.CharField(max_length=100)
+    slug=models.SlugField(unique=True)
+    parent=models.ForeignKey('self',on_delete=models.CASCADE,related_name='category')  #for reverse query as you can access the category even as a sub category fi ever needed to do so
+    
+    def __str__(self):
+        return  self.cat_name
+    
+class Products(models.Model):
+    product_name=models.CharField(max_length=50)
+    product_description=models.TextField(max_length=100,blank=True,null=True)
+    brand=models.CharField(max_length=50)
+    created_at=models.DateTimeField(auto_now_add=True)
+    category=models.Fore
+    #string representation in the superadmin
+    def __str__(self):
+        return self.product_name
+
+class vendorProduct(models.Model):
+    vendor=models.ForeignKey(Vendor,on_delete=models.CASCADE)
+    product=models.ForeignKey(Products,on_delete=models.CASCADE)  
+    price=models.FloatField()
+    stock=models.IntegerField(default=0)
+    listed_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.vendor.store_name} - {self.product.name}"
+      
